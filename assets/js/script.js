91267cad516ccb496023644dc22edadfc7429421
gsr142 <gsr142@gmail.com>
2023-07-07 15:56:37 -0700

    add date and time to top of page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready(function () {
+
+  var now = dayjs().format('MMMM D, YYYY. h:m a');
+  $('#currentDay').text(now);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
