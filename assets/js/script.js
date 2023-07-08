aec828507a85ee2dffc4130223ffa3fb0705307c
gsr142 <gsr142@gmail.com>
2023-07-08 15:11:46 -0700

    fix save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 // in the html.
 $(document).ready(function () {
 
-  var now = dayjs().format('MMMM D, YYYY. h:m a');
+  var now = dayjs().format('MMMM D, YYYY. h:mm a');
   $('#currentDay').text(now);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -11,15 +11,16 @@ $(document).ready(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  $('.btn').click (function() {
-    var text = $('.btn').siblings('.description').val();
-    //next line not working properly
-    var taskStorage = $('.btn').siblings('.hour').text();
+  $('.btn').click(function() {
+    var text = $(this).siblings('.description').val();
+    var taskStorage = $(this).siblings('.hour').text();
     localStorage.setItem(taskStorage, text);
+    console.log(taskStorage)
     console.log(text);
     //alert("Task Saved");
   });
 
+  
 
 
 
@@ -49,5 +50,5 @@ $(document).ready(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  
 });
