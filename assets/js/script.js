0d9b38f134e9a858d6f536a9634dfbf801c45064
gsr142 <gsr142@gmail.com>
2023-07-08 15:32:42 -0700

    add function to retrieve data from local storage and display in the proper time block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,23 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(document).ready(function () {
 
-  var now = dayjs().format('MMMM D, YYYY. h:mm a');
+  var now = dayjs().format('dddd MMMM D, YYYY. h:mm a');
   $('#currentDay').text(now);
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+ 
+
   $('.btn').click(function() {
     var text = $(this).siblings('.description').val();
     var taskStorage = $(this).siblings('.hour').text();
     localStorage.setItem(taskStorage, text);
-    console.log(taskStorage)
-    console.log(text);
-    //alert("Task Saved");
+    alert("Task Saved");
   });
 
   
+  $('.description').each(function() {
+    taskStorage = $(this).siblings('.hour').text();
+    text = localStorage.getItem(taskStorage);
+    $(this).text(text);
+  });
 
 
 
